5fe5fd7e9a3d37bfbfd4f221de99fcdfb02f4ed3
GraffPhoneix <joraasatryan.ait@gmail.com>
2025-06-09 20:24:54 +0400

    Images for news
    Added Images for news

--- team-telecom/src/Components/News-item.jsx
+++ team-telecom/src/Components/News-item.jsx
@@ -1,9 +1,10 @@
-export default function NewsItem({ title, body, date, id }) {
-  return (
-    <div className="bg-gray-100 rounded-2xl shadow-md p-6 hover:shadow-lg transition-shadow duration-300" key={id}>
-      <h3 className="text-xl font-semibold text-red-600">{title}</h3>
-      <p className="text-sm text-gray-500 mt-1">{date}</p>
-      <p className="mt-2 text-gray-700">{body}</p>
-    </div>
-  );
+export default function NewsItem({ title, body, date, id, imgURL }) {
+    return (
+        <div className="bg-gray-100 rounded-2xl shadow-md p-6 hover:shadow-lg transition-shadow duration-300" key={id}>
+            <img alt='data' src={imgURL} className="rounded-2xl" />
+            <h3 className="text-xl font-semibold text-red-600">{title}</h3>
+            <p className="text-sm text-gray-500 mt-1">{date}</p>
+            <p className="mt-2 text-gray-700">{body}</p>
+        </div>
+    );
 }
--- team-telecom/src/Components/News.jsx
+++ team-telecom/src/Components/News.jsx
@@ -14,11 +14,11 @@ for (let i = 0; i < dataBase.News.length; i += 2) {
 export default function News() {
     return (
         <section className="bg-white py-10 px-4 md:px-16">
-            <h2 className="text-3xl font-bold text-gray-800 mb-6">Новости</h2>
-            <Swiper modules={[Navigation, Pagination]} navigation  spaceBetween={20} className="pb-10" >
+            <h2 className="text-3xl font-bold text-gray-800 mb-6 bg-center">Новости</h2>
+            <Swiper modules={[Navigation, Pagination]} navigation spaceBetween={20} className="pb-10" >
                 {slides.map((group, index) => (
                     <SwiperSlide key={index}>
-                        <div className="grid gap-6 md:grid-cols-2">{group.map((el) => <NewsItem id={el.id} title={el.title} body={el.body} date={el.date} />)}</div>
+                        <div className="grid gap-6 md:grid-cols-2">{group.map((el) => <NewsItem id={el.id} title={el.title} body={el.body} date={el.date} imgURL={el.imgURL} />)}</div>
                     </SwiperSlide>
                 ))}
             </Swiper>
